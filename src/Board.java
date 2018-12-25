999e8afa1bd303b3867f2d62a28a590f758e4c69
MiraPratt <joseph.pratt@case.edu>
2018-12-25 02:06:20 -0500

    Working Game

--- src/Board.java
+++ src/Board.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 
 public class Board extends JPanel{
 	
-	private char turn = 'x';
+	private String turn = "x";
 	
 	private Tile[][] tiles;
 	
@@ -24,14 +24,26 @@ public class Board extends JPanel{
 			
 			for(int j = 0 ; j < 3 ; j++) {
 	
-				tiles[j][i] = new Tile(j,i);
-				this.add(tiles[j][i]);
+				getTiles()[j][i] = new Tile(j,i);
+				this.add(getTiles()[j][i]);
 	
-				tiles[j][i].addActionListener(new ActionListener(){
+				getTiles()[j][i].addActionListener(new ActionListener(){
 							
 					public void actionPerformed(ActionEvent e) {
 						Tile t = (Tile)e.getSource();	
 						mark(t);
+						int result = outcome();
+						int[] r = toArray();
+						System.out.println(r[0] + "" + r[1] + r[2]);
+						if(result > 0) {
+							if(result == 1) {
+								getTiles()[1][1].setText("x won");
+							}
+							else {
+								getTiles()[1][1].setText("o won");
+							}
+								
+						}
 					}
 							
 				});
@@ -42,6 +54,82 @@ public class Board extends JPanel{
 	    
 	}
 
+	public int outcome() {
+		int[] arr = toArray();
+		int r = 0;
+	    if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[3] == 1 && arr[4] == 1 && arr[5] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[6] == 1 && arr[7] == 1 && arr[8] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[0] == 1 && arr[4] == 1 && arr[8] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[2] == 1 && arr[4] == 1 && arr[6] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[0] == 1 && arr[3] == 1 && arr[6] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[1] == 1 && arr[4] == 1 && arr[7] == 1) {
+	    	r = 1;
+	    }
+	    if(arr[2] == 1 && arr[5] == 1 && arr[8] == 1) {
+	    	r = 1;
+	    }
+	    
+	    
+	    if(arr[0] == 2 && arr[1] == 2 && arr[2] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[3] == 2 && arr[4] == 2 && arr[5] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[6] == 2 && arr[7] == 2 && arr[8] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[0] == 2 && arr[4] == 2 && arr[8] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[2] == 2 && arr[4] == 2 && arr[6] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[0] == 2 && arr[3] == 2 && arr[6] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[1] == 2 && arr[4] == 2 && arr[7] == 2) {
+	    	r = 2;
+	    }
+	    if(arr[2] == 2 && arr[5] == 2 && arr[9] == 2) {
+	    	r = 2;
+	    }
+	    return r;
+	    
+	}
+	
+	
+	
+	
+	public int[] toArray() {
+		int[] arr = new int[9];
+		
+		for(int i = 0; i < 3 ; i++) {
+			for(int j = 0; j < 3 ; j++) {
+				if(getTiles()[j][i].getText() == "x"){
+					arr[3 * i + j] = 1;
+				}
+				if(getTiles()[j][i].getText() == "o"){
+					arr[3 * i + j] = 2;
+				}
+			}
+		}
+		return arr;
+	}
+	
 	
 	public void mark(JButton t) {
 		if(t.getText() == "") {
@@ -51,14 +139,18 @@ public class Board extends JPanel{
 	}
 	
 	public void changeTurn() {
-		if(getTurn() == 'x')
-			turn = 'o';
+		if(getTurn() == "x")
+			turn = "o";
 		else
-			turn = 'x';
+			turn = "x";
 	}
 	
-	public char getTurn() {
+	public String getTurn() {
 		return turn;
+	}
+	
+	public Tile[][] getTiles(){
+		return tiles;
 	}
 	
 }
