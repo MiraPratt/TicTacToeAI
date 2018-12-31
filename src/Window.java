9d4d4c56d7a6bd919ed6bbdd13c63ab9a22e2b1d
Joseph Pratt <jpp85@case.edu>
2018-12-31 01:17:00 -0500

    Removed Unnecessary Class

--- src/Window.java
+++ src/Window.java
@@ -2,33 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
+/**
+ * @author Mira Pratt
+ * This class is a window that contains 
+ * the buttons which represent the squares for tictactoe
+ * */
 public class Window extends JFrame {
 
-	private Board b = new Board();
-	
-	public Window() {
-		this.setSize(600,600);
-		this.getContentPane().add(b);
-		this.setTitle("TIC-TAC-TOE");
-		this.setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		
-		Window win = new Window();
-		
-		//testing a board state
-		/*
-		win.getBoard().getTiles()[0][0].setText("x");
-		win.getBoard().getTiles()[1][1].setText("o");
-		win.getBoard().getTiles()[2][1].setText("x");
-		win.getBoard().getTiles()[2][2].setText("o");
-		*/
-	}
-	
-	public Board getBoard() {
-		return b;
-	}
-	
+ private Board b = new Board();
+ 
+ public Window() {
+  this.setSize(600,600);
+  this.getContentPane().add(b);
+  this.setTitle("TIC-TAC-TOE");
+  this.setVisible(true);
+ }
+ 
+ public static void main(String[] args) {
+  
+  Window win = new Window();
+  
+ }
+ 
+ public Board getBoard() {
+  return b;
+ }
+ 
 }
